4eccc714dc8f46e630cd9c07fc0cccdd14b60885
Xander Geurts <xander.geurts@rocdeleijgraaf.nl>
2020-12-03 12:12:57 +0100

    reken generater stage 1 uncompleted

---
+++ groep5.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>RekenWebsite</title>
+    <link rel="stylesheet" href="stijl.css">
+
+</head>
+<body>
+<script>
+    function playAudio(url) {
+        new Audio(url).play();
+    }
+</script>
+
+
+<div class="sidenav">
+
+
+
+    <img src="bestanden/logo.jpg" alt="logo de splinter" WIDTH="95" HEIGHT="100" class="pica" onclick="playAudio('Initial D Spitfire normal.mp3')">
+    <a href="rekenwebsite.php">home</a>
+    <a href="info.php">info</a>
+    <a href="oefenen.php">oefenen</a>
+    <a href="contact.php">contact</a>
+</div>
+
+<div class="main">
+
+    <div class="container col-md-3 left-column">
+        <div class="card">
+            <div class="card-body">
+                <fieldset>
+                    <hr />
+                    <h2 class="card-title">  Oefensommen Groep 5</h2>
+                    <hr />
+                </fieldset>
+
+            </div>
+        </div>
+    </div>
+
+    <?php
+
+    $min = 0;
+    $max = 100;
+
+    $getal1 = rand ($min , $max );
+
+    $getal2 = rand ($min , $max );
+
+    $randomScore = rand(1,4); // random score between 0-4
+    switch ($randomScore) {
+
+        case 1:
+            $uitkomst = $getal1 * $getal2;
+            echo "hoeveel is $getal1 * $getal2";
+            echo "<br/>";
+            break;
+        case 2:
+            $uitkomst = $getal1 / $getal2;
+            echo "hoeveel is $getal1 / $getal2";
+            break;
+        case 3:
+            $uitkomst = $getal1 + $getal2;
+            echo "hoeveel is $getal1 + $getal2";
+            break;
+        case 4:
+            $uitkomst = $getal1 - $getal2;
+            echo "hoeveel is $getal1 - $getal2";
+            break;
+    }
+
+    echo "<br />";
+  echo $uitkomst;
+
+    ?>
+
+
+</body>
+
+</html>
